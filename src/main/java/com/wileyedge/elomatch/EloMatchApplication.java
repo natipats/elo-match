cd83143bfc61277e5e66d697872a78893724e203
natipats <zaparods73@gmail.com>
2023-05-25 18:59:42 +0100

    Initial commit

---
+++ src/main/java/com/wileyedge/elomatch/EloMatchApplication.java
@@ -0,0 +1,13 @@
+package com.wileyedge.elomatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EloMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EloMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wileyedge/elomatch/EloMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wileyedge.elomatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EloMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
