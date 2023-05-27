f534999121256999a2c0d78cebba64a7095ec30c
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-27 16:21:30 +0100

    sequence value generated with a default of 1 and the stratey of this generator is a sequence

--- src/main/java/com/wileyedge/elomatch/model/UserModel.java
+++ src/main/java/com/wileyedge/elomatch/model/UserModel.java
@@ -6,8 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
 @Getter
 @Setter
+// This means that it is now an identity
+// and it is now mapped to a database.
 @Entity
 @Table(name="user")
 @NoArgsConstructor
@@ -17,7 +21,18 @@ public class UserModel {
     // This will represent the entity in the database.
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // sequence will increase by 1 as default value.
+    // So it'll start at 1 counting up to 2, 3, 4, 5, 6, 7, 8, 9, 10 etc
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    // the generated value will be a sequence.
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "user_sequence"
+    )
     @Column(name = "userid")
     private int userid;
     @Column(name = "playerName")
