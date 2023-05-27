6c6e01e1fe25f3e578bcbf36dfeb57de0c5d5085
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-27 17:34:26 +0100

    Declare final UserServiceInterface variable

--- src/main/java/com/wileyedge/elomatch/controller/UserController.java
+++ src/main/java/com/wileyedge/elomatch/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.wileyedge.elomatch.controller;
 
+import com.wileyedge.elomatch.service.UserServiceInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
+    //declares a constant variable named userServiceInterface
+    // of type UserServiceInterface that cannot be changed once it is set.
+    private final UserServiceInterface userServiceInterface;
 }
