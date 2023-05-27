ddfebdfa2ce94b07b1eb131ce60c4d5ecd7eae5b
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-27 18:14:19 +0100

    Define JpaRepository for UserModel and Long

--- src/main/java/com/wileyedge/elomatch/persistence/UserRepository.java
+++ src/main/java/com/wileyedge/elomatch/persistence/UserRepository.java
@@ -4,14 +4,16 @@ import com.wileyedge.elomatch.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
 
 // The @Repository annotation indicates that this interface is a repository,
 // allowing it to be automatically detected and used by the framework.
 
-// The JpaRepository<UserModel, UUID> specifies that UserModel is the entity class managed
-// by this repository, and UUID is the type of the entity's identifier (primary key).
+//The JpaRepository<UserModel, Long> represents a generic interface that provides the functionality
+// for accessing and managing data in the context of a Spring Data JPA project.
+
+// By extending JpaRepository<UserModel, Long>, the repository gains pre-defined methods
+// for common database operations, such as creating, reading, updating, and deleting.
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, UUID> {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 }
