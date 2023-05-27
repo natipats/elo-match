d9e3a3fc532c474ffe0af327ba0ac15d743b438a
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-27 14:31:35 +0100

    annotating the Table as name User and the generative value

--- src/main/java/com/wileyedge/elomatch/model/UserModel.java
+++ src/main/java/com/wileyedge/elomatch/model/UserModel.java
@@ -1,13 +1,23 @@
 package com.wileyedge.elomatch.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Columns;
+import org.hibernate.annotations.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name="user")
 public class UserModel {
 
-    private int elo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userID;
+    @Column()
+    private long elo;
+    @Column(name="henry")
     private String playerName;
     private String userName;
     private boolean isToxic;
