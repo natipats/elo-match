b64be11f4f1bf9df4e99ea1409dc38a03b73a350
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-28 00:51:14 +0100

    annotations for service of ranking

--- src/main/java/com/wileyedge/elomatch/service/RankServiceImpl.java
+++ src/main/java/com/wileyedge/elomatch/service/RankServiceImpl.java
@@ -1,4 +1,16 @@
 package com.wileyedge.elomatch.service;
 
-public class RankServiceImpl {
+import com.wileyedge.elomatch.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Component
+public class RankServiceImpl implements RankServiceInterface {
+
+    @Autowired
+    private UserRepository rankingAttached;
 }
