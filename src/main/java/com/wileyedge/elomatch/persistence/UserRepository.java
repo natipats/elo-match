907bcc17688ba8a3a56daaa49017edf24bf4296f
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-27 17:50:59 +0100

    Add UserRepository interface

--- src/main/java/com/wileyedge/elomatch/persistence/UserRepository.java
+++ src/main/java/com/wileyedge/elomatch/persistence/UserRepository.java
@@ -1,4 +1,17 @@
 package com.wileyedge.elomatch.persistence;
 
-public interface UserRepository {
+import com.wileyedge.elomatch.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+// The @Repository annotation indicates that this interface is a repository,
+// allowing it to be automatically detected and used by the framework.
+
+// The JpaRepository<UserModel, UUID> specifies that UserModel is the entity class managed
+// by this repository, and UUID is the type of the entity's identifier (primary key).
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
 }
