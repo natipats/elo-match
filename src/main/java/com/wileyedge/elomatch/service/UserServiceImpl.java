5374b0d3fc312aab980cb8829353de0ee4979d46
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-28 00:43:16 +0100

    annotations included for service

--- src/main/java/com/wileyedge/elomatch/service/UserServiceImpl.java
+++ src/main/java/com/wileyedge/elomatch/service/UserServiceImpl.java
@@ -1,4 +1,17 @@
 package com.wileyedge.elomatch.service;
 
-public class UserServiceImpl {
+import com.wileyedge.elomatch.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Component
+public class UserServiceImpl implements UserServiceInterface {
+
+    @Autowired
+    private final UserRepository userRepository;
+
 }
