223f13c48be15a80bb2817f2cd6d4b4b168ef307
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-27 17:27:12 +0100

    Declare final RankServiceInterface variable

--- src/main/java/com/wileyedge/elomatch/controller/RankController.java
+++ src/main/java/com/wileyedge/elomatch/controller/RankController.java
@@ -1,9 +1,17 @@
 package com.wileyedge.elomatch.controller;
 
+
+import com.wileyedge.elomatch.service.RankServiceInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
+// Add the @RestController annotation to indicate it's a special controller
 @RestController
+// Add the @RequiredArgsConstructor annotation to generate a constructor for dependencies
 @RequiredArgsConstructor
 public class RankController {
+
+    //declares a constant variable named rankServiceInterface
+    // of type RankServiceInterface that cannot be changed once it is set
+    private final RankServiceInterface rankServiceInterface;
 }
