9fca2e614b84239eaae4327d4a8568e33430a024
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-28 00:46:35 +0100

    annotation for rankingId and columns created with one to many database connecting with the user

--- src/main/java/com/wileyedge/elomatch/model/RankModel.java
+++ src/main/java/com/wileyedge/elomatch/model/RankModel.java
@@ -1,12 +1,28 @@
 package com.wileyedge.elomatch.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
+
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class RankModel {
 
+    @Id
+    @Column(name = "ranking_id", nullable = false)
+    private Integer ranking_id;
+    @Column(name = "exp")
     private String exp;
-    private int rankID;
+
+    @OneToMany(mappedBy = "ranking", cascade = CascadeType.ALL)
+    private List<UserModel> userid;
+
 
 }
