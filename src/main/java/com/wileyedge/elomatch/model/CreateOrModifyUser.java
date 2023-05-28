d4347d2447eeb932da9c7b90aa4c6d9a630cbb3e
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-29 00:57:29 +0100

    created model package with class of CreateOrModifyUser

--- src/main/java/com/wileyedge/elomatch/model/CreateOrModifyUser.java
+++ src/main/java/com/wileyedge/elomatch/model/CreateOrModifyUser.java
@@ -1,6 +1,5 @@
 package com.wileyedge.elomatch.model;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +9,13 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class ModifyUser {
+public class CreateOrModifyUser {
 
+    // you only need the playerName and UserName to modify the user
+    // you wouldn't allow the user to change rank or change the istoxic player otherwise
+    // they could cheat.
+    // they can also create there own user identity to start a match
+    // which will identify them as beginner status.
     private String playerName;
     private String userName;
 }
