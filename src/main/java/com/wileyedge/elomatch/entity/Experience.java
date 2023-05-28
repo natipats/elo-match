040fa30da1ad157da2b1db645e95f6e72bcda654
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-29 00:45:25 +0100

    comments to explain code

--- src/main/java/com/wileyedge/elomatch/entity/Experience.java
+++ src/main/java/com/wileyedge/elomatch/entity/Experience.java
@@ -10,19 +10,20 @@ import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // generate constructors without any arguments
+@AllArgsConstructor // generate constructors with all arguments, respectively.
 @Entity
 @Table(name = "experience")
 public class Experience {
 
-    @Id
+    @Id // specifies the primary key column in the database table.
     @Column(name = "id", nullable = false)
     private Long id;
-    @Column(name = "exp")
+    @Column(name = "exp") // used to map the attributes to specific columns in the table.
     private String exp;
 
-
+    //  establishes a one-to-many relationship with the User entity,
+    //  specifying that one experience can be associated with multiple users.
     @OneToMany(mappedBy = "experience", cascade = CascadeType.ALL)
     private List<User> users;
 
