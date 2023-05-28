5a1a5077deeba0ad60f81974ddc867af48c45759
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-29 00:36:05 +0100

    Added ExperienceViewController class with updated name

--- src/main/java/com/wileyedge/elomatch/controller/ExperienceController.java
+++ src/main/java/com/wileyedge/elomatch/controller/ExperienceController.java
@@ -1,7 +1,7 @@
 package com.wileyedge.elomatch.controller;
 
 
-import com.wileyedge.elomatch.service.RankService;
+import com.wileyedge.elomatch.service.ExperienceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 // Add the @RequiredArgsConstructor annotation to generate a constructor for dependencies
 @RequiredArgsConstructor
-public class RankController {
+public class ExperienceController {
 
     //declares a constant variable named rankServiceInterface
     // of type RankServiceInterface that cannot be changed once it is set
-    private final RankService rankService;
+    private final ExperienceService experienceService;
 }
