1ab4dae52c3377c8559d8ad32611871f2d30a7ad
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-29 01:14:21 +0100

    comment explaining what the ExperienceViewController acts as

--- src/main/java/com/wileyedge/elomatch/view/ExperienceViewController.java
+++ src/main/java/com/wileyedge/elomatch/view/ExperienceViewController.java
@@ -8,5 +8,12 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class ExperienceViewController {
 
+    /*
+    the ExperienceViewController acts as a controller that relies on the ExperienceService
+    to perform operations related to experiences. By using constructor injection,
+    the ExperienceService instance is automatically provided to the controller,
+    allowing it to access the necessary functionality to handle HTTP requests
+    and process the associated business logic.
+     */
     private final ExperienceService experienceService;
 }
