65b29d7249e5d9067e2e4fa22a959c30a9c5ea51
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-05-30 21:04:21 +0100

    comment on elo and isToxic for create and modify data

--- src/main/java/com/wileyedge/elomatch/model/CreateOrModifyUserModel.java
+++ src/main/java/com/wileyedge/elomatch/model/CreateOrModifyUserModel.java
@@ -14,10 +14,11 @@ public class CreateOrModifyUserModel {
     // you only need the playerName and UserName to modify the user
     // you wouldn't allow the user to change rank or change the istoxic player otherwise
     // they could cheat.
-    // they can also create there own user identity to start a match
+    // they can also create their own user identity to start a match
     // which will identify them as beginner status.
     private String playerName;
     private String userName;
+    // For now, I have added elo and isToxic for insert data.
     private Long elo;
     private Boolean isToxic;
 }
