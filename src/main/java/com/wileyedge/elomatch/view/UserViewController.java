a67fcdc95e5058a6441e24363baa8dd1adeec685
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-06-05 16:26:25 +0100

    created a GetMapping for routing to signup on frontend and using addNewUser method

--- src/main/java/com/wileyedge/elomatch/view/UserViewController.java
+++ src/main/java/com/wileyedge/elomatch/view/UserViewController.java
@@ -1,13 +1,12 @@
 package com.wileyedge.elomatch.view;
 
+import com.wileyedge.elomatch.entity.User;
 import com.wileyedge.elomatch.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,12 +25,12 @@ public class UserViewController {
         return "users";
     }
 
-    @PostMapping("/users")
-    public String addNewUser(){
-
-
-
-        }
-    @GetMapping("/register")
+    @GetMapping("/signup")
+    public String addNewUser(Model model){
+        // create model object to store form data
+        User user = new User();
+        model.addAttribute("user", user);
+        return "signup";
+    }
 
 }
