2828e7ed1eb2f062fcf50e254c673fb729e6b5d7
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-06-05 17:37:00 +0100

    Added controller methods for user sign-up and saving new users to the database.

--- src/main/java/com/wileyedge/elomatch/view/UserViewController.java
+++ src/main/java/com/wileyedge/elomatch/view/UserViewController.java
@@ -6,7 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,6 +35,30 @@ public class UserViewController {
         User user = new User();
         model.addAttribute("user", user);
         return "signup";
+    }
+
+    @PostMapping("/signup/save")
+    public String addingThisNewUserToDatabase(@Validated @ModelAttribute("user")User user,
+                                                       BindingResult result,
+                                                       Model model) {
+        User userSubmitAndAdded = userService.findUserByName(user.getPlayerName());
+        userSubmitAndAdded = userService.findUserByName(user.getUserName());
+
+        if(userSubmitAndAdded != null && userSubmitAndAdded.getPlayerName() != null && !userSubmitAndAdded.getPlayerName().isEmpty()){
+            result.rejectValue("playerName", null, "ERROR: this player name does not exist");
+        }
+
+        if(userSubmitAndAdded != null && userSubmitAndAdded.getUserName() != null && !userSubmitAndAdded.getUserName().isEmpty()){
+            result.rejectValue("UserName", null, "ERROR: this user name does not exist");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", user);
+            return "/signup";
+        }
+
+        userService.saveUser(user);
+        return "redirect:/users";
     }
 
 }
